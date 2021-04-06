7f3ddb50deec9a40231309a0dc38aad4512a2223
Jonathan Albert <JonathanAlbert0115@gmail.com>
2021-04-06 02:40:55 -0400

    Added balloons to account on deploy

--- packages/buidler/contracts/DEX.sol
+++ packages/buidler/contracts/DEX.sol
@@ -4,12 +4,24 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DEX {
+    using SafeMath for uint256;
+    IERC20 token;
 
-  using SafeMath for uint256;
-  IERC20 token;
+    constructor(address token_addr) public {
+        token = IERC20(token_addr);
+    }
 
-  constructor(address token_addr) public {
-    token = IERC20(token_addr);
-  }
+    //tracks total liquidity
+    uint256 public totalLiquidity;
+    //tracks individual addresses
+    mapping(address => uint256) public liquidity;
 
+    //Loads contracts with ETH and balloons
+    function init(uint256 tokens) public payable returns (uint256) {
+        require(totalLiquidity == 0, "DEX:init - already has liquidity");
+        totalLiquidity = address(this).balance;
+        liquidity[msg.sender] = totalLiquidity;
+        require(token.transferFrom(msg.sender, address(this), tokens));
+        return totalLiquidity;
+    }
 }
